5a041489380b773c33f46b2fe518b360ac42ff3c
Sujeet Akula <sujeet@freeboson.org>
2017-03-09 22:02:32 +1100

    few more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,7 @@ using Base.Test
 srand(0)
 
 @test xkpasswd.generate(4) == ["ups flux photos bits 4"]
+@test xkpasswd.generate(4)[1] == "economies centers dresses history 8"
+@test xkpasswd.generate(8,npws=7,capitalize=true,delimstr="-")[3] == "Reservations-Construct-Conferencing-Hundred-Rx-Gd-Inside-Payroll-8"
+
+
