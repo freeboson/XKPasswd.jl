e636a2612bee2f08ffd2806a1774523eda39fe66
Sujeet Akula <sujeet@freeboson.org>
2017-03-09 17:52:09 +1100

    setup wordlist path

--- src/xkpasswd.jl
+++ src/xkpasswd.jl
@@ -1,8 +1,14 @@
 module xkpasswd
 
+datapath = joinpath(@__DIR__, "..", "data")
+default_wordlist = joinpath(datapath, "google-10000-english",
+                            "google-10000-english-usa-no-swears.txt")
+
 pwentropy(n, total, digit) = n*log2(total) + (digit ? log2(10) : 0)
 
-function generate(n::Integer; npws::Integer=1, capitalize::Bool=false, wordlist::AbstractString="google-10000-english-usa-no-swears.txt", delimstr::AbstractString=" ", append_digit::Bool=true)
+function generate(n::Integer; npws::Integer=1, capitalize::Bool=false,
+                  wordlist::AbstractString=default_wordlist,
+                  delimstr::AbstractString=" ", append_digit::Bool=true)
     @assert n > 0 "Must use at least 1 random word"
     @assert npws > 0 "Must generate at least 1 password"
     open(wordlist) do f
