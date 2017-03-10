dab802a2deaf71c801e6e87c85630c3389133fbb
Sujeet Akula <sujeet@freeboson.org>
2017-03-10 13:41:31 +1100

    nicer handling of word lists

--- src/xkpasswd.jl
+++ src/xkpasswd.jl
@@ -1,19 +1,39 @@
 module xkpasswd
 
 datapath = joinpath(@__DIR__, "..", "data")
-default_wordlist = joinpath(datapath, "google-10000-english",
-                            "google-10000-english-usa-no-swears.txt")
+google10kpath = joinpath(datapath, "google-10000-english")
 
 pwentropy(n, total, digit) = n*log2(total) + (digit ? log2(10) : 0)
 
-function generate(n::Integer; npws::Integer=1, capitalize::Bool=false,
-                  wordlist::AbstractString=default_wordlist,
-                  delimstr::AbstractString=" ", append_digit::Bool=true)
+@enum WordList simple jargon google_20k google_10k google_10k_clean google_10k_usa google_10k_usa_clean google_10k_usa_clean_short google_10k_usa_clean_medium google_10k_usa_clean_long
+
+wordlistfiles = Dict(simple => joinpath(datapath, "simple.txt"),
+                     jargon => joinpath(datapath, "jargon.txt"),
+                     google_20k => joinpath(google10kpath, "20k.txt"),
+                     google_10k => joinpath(google10kpath,
+                                            "google-10000-english.txt"),
+                     google_10k_clean => joinpath(google10kpath,
+                         "google-10000-english-no-swears.txt"),
+                     google_10k_usa => joinpath(google10kpath,
+                         "google-10000-english-usa.txt"),
+                     google_10k_usa_clean => joinpath(google10kpath,
+                         "google-10000-english-usa-no-swears.txt"),
+                     google_10k_usa_clean_short => joinpath(google10kpath,
+                         "google-10000-english-usa-no-swears-short.txt"),
+                     google_10k_usa_clean_medium => joinpath(google10kpath,
+                         "google-10000-english-usa-no-swears-medium.txt"),
+                     google_10k_usa_clean_long => joinpath(google10kpath,
+                         "google-10000-english-usa-no-swears-long.txt"))
+
+function generate(n::Integer, wordlist::AbstractString; npws::Integer=1,
+                  capitalize::Bool=false, delimstr::AbstractString=" ",
+                  append_digit::Bool=true)
     @assert n > 0 "Must use at least 1 random word"
     @assert npws > 0 "Must generate at least 1 password"
     open(wordlist) do f
         lines = readlines(f, chomp=true)
-        println(STDERR, "Estimated entropy: ~$(pwentropy(n,length(lines),append_digit) |> round |> Int) bits.")
+        println(STDERR, "Estimated entropy: ",
+                "~$(round(Int, pwentropy(n,length(lines),append_digit))) bits.")
         [(
             words = rand(lines, n);
             cased_words = capitalize ?  map(ucfirst, words) : words;
@@ -23,4 +43,12 @@ function generate(n::Integer; npws::Integer=1, capitalize::Bool=false,
     end
 end
 
+function generate(n::Integer, wordlist::WordList=simple; npws::Integer=1,
+                  capitalize::Bool=false, delimstr::AbstractString=" ",
+                  append_digit::Bool=true)
+    generate(n, wordlistfiles[wordlist], npws=npws, capitalize=capitalize,
+             delimstr=delimstr, append_digit=append_digit)
+end
+
 end # module
+
