7dafeb32f44cba47f4149be30711a2e938942a5e
Sujeet Akula <sujeet@freeboson.org>
2017-03-10 14:19:37 +1100

    update/add tests for new behavior

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,13 @@
+
 using xkpasswd
 using Base.Test
 
-srand(0)
+srand(0);
 
-@test xkpasswd.generate(4) == ["ups flux photos bits 4"]
-@test xkpasswd.generate(4)[1] == "economies centers dresses history 8"
-@test xkpasswd.generate(8,npws=7,capitalize=true,delimstr="-")[3] == "Reservations-Construct-Conferencing-Hundred-Rx-Gd-Inside-Payroll-8"
-
+@test xkpasswd.pwentropy(4, 2048, false) == 44.0
+@test xkpasswd.generate(4) == ["simple curtain forgive society 4"]
+@test xkpasswd.generate(4)[1] == "we besides someone iron 8"
+@test xkpasswd.generate(8,npws=7,capitalize=true,delimstr="-")[3] == "Steep-Paste-Over-Bag-Wall-Flag-Scatter-Theater-8"
+@test xkpasswd.generate(6, joinpath(Pkg.dir("xkpasswd"), "data", "jargon.txt"), append_digit=false) == ["robots notesfiles listener overflowed reflects dissemination"]
+@test xkpasswd.generate(4, xkpasswd.google_10k_clean)[1] == "disturbed saver palm sapphire 7"
 
