218375d711edc70076be24cdd91fdfe495507bb2
Sujeet Akula <sujeet@freeboson.org>
2017-03-23 13:26:32 +1100

    add some doc strings

--- src/XKPasswd.jl
+++ src/XKPasswd.jl
@@ -1,5 +1,23 @@
 module XKPasswd
 
+"""
+`XKPasswd` generates memorable passwords for you in the style of diceware and
+[this xkcd comic](https://xkcd.com/936/). Note: diceware fairly explicitly tells
+you to use dice for randomness and *not* a PRNG, so keep that in mind.
+
+This module does not export symbols but the main functions of interest are:
+
+- `XKPasswd.generate`: gives an array of generated passwords
+- `XKPasswd.pwentropy`: gives the bits of entropy in the password, with
+  knowledge of the wordlist used and formatting
+- `XKPasswd.pw100yattrate`: estimates the guess attempt rate needed to brute
+  force the password within 100 years, with knowledge of the wordlist used and
+  formatting
+- `XKPasswd.spin_the_wheel`: cycles through random passwords until you stop it
+  (should be used from REPL or a terminal)
+"""
+XKPasswd
+
 using Base.prettyprint_getunits
 
 const superunits = ["", " k", " M", " G", " T", " P", " E", " Z", " Y"]
@@ -41,6 +59,30 @@ wordlistfiles = Dict(simple => joinpath(datapath, "simple.txt"),
                      google_10k_usa_clean_long => joinpath(google10kpath,
                          "google-10000-english-usa-no-swears-long.txt"))
 
+"""
+    XKPasswd.generate(n[, wordlist=XKPasswd.simple]; <keyword arguments>)
+
+Generate an array of memorable passwords of a given specification.
+
+# Arguments
+* `n::Integer`: the number of random words per password
+* `wordlist::WordList=XKPasswd.simple`: selection from the bundled word list options OR
+* `wordlist::AbstractString`: the path to a newline delimited word list file
+* `npws::integer=1`: the number of passwords to generate
+* `captialize::Bool=false`: whether to capitalize the first letter of each word
+* `delimstr::AbstractString=" "`: what to insert between each word
+* `append_digit::Bool=true`: whether to tack on a single digit at the end
+* `quiet::Bool=false`: whether to print entropy information to STDERR
+
+# Examples
+
+```julia
+julia> XKPasswd.generate(4, append_digit=false) # output here is just a joke
+Entropy: ~45 bits; 100y BF attempt rate: 9 k att/s.
+1-element Array{String,1}:
+ "correct horse battery staple"
+```
+"""
 function generate(n::Integer, wordlist::AbstractString; npws::Integer=1,
                   capitalize::Bool=false, delimstr::AbstractString=" ",
                   append_digit::Bool=true, quiet::Bool=false)
@@ -69,11 +111,20 @@ end
 
 function generate(n::Integer, wordlist::WordList=simple; npws::Integer=1,
                   capitalize::Bool=false, delimstr::AbstractString=" ",
-                  append_digit::Bool=true, quiet::Bool=true)
+                  append_digit::Bool=true, quiet::Bool=false)
     generate(n, wordlistfiles[wordlist], npws=npws, capitalize=capitalize,
              delimstr=delimstr, append_digit=append_digit, quiet=quiet)
 end
 
+"""
+    XKPasswd.spin_the_wheel(n::Integer=4; <keyword arguments>)
+
+Starts cycling through randomly generated passwords, selecting the last one once
+you hit [Enter] on your keyboard. Arguments are the same as in
+`XKPasswd.generate`, though without `npws` and `quiet`. NOTE: this function puts
+VT100 commands to STDOUT and reads from file descriptor 0, so it might not be
+applicable in all situations.
+"""
 function spin_the_wheel(n::Integer=4, wordlist::WordList=simple;
                         capitalize::Bool=false, delimstr::AbstractString=" ",
                         append_digit::Bool=true)
